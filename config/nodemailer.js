760b9952055b936d59f1f7b7c43366dcb797da6e
Harshitha187 <harshithayenigalla@gmail.com>
2025-09-25 13:08:38 +0530

    fixed otp issue

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -16,12 +16,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
-  },
-  connectionTimeout: 35000, // 35 seconds
-  greetingTimeout: 25000, // 25 seconds
-  socketTimeout: 35000, // 35 seconds
-  debug: false, // Disable debug for performance
-  logger: false // Disable logging for performance
+  }
 });
 
 export default transporter;
